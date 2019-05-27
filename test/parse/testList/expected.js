843a62c8549139031ea4fed9c44492064671dfa1
Sejin Jeon <jinaidy93@gmail.com>
2019-05-27 12:28:53 +0900

    Add testcase files for parse test

---
+++ test/parse/testList/expected.js
@@ -0,0 +1,91 @@
+export default [
+  [
+    {
+      type: 'rest',
+      content: `:checkered_flag: :grapes:
+  :thought_balloon:`,
+    },
+    {
+      type: 'comment',
+      content: ' Get things up and running here...',
+    },
+    {
+      type: 'rest',
+      content: `
+  :grinning: "`,
+    },
+    {
+      type: 'string',
+      content: 'Hello World!',
+    },
+    {
+      type: 'rest',
+      content: `:abc:;
+:watermelon:
+`,
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: 'main {\n  var ',
+    },
+    {
+      type: 'identifier',
+      content: 'pet_name',
+    },
+    {
+      type: 'rest',
+      content: ' :string  //',
+    },
+    {
+      type: 'comment',
+      content: ' I love cats.',
+    },
+    {
+      type: 'rest',
+      content: `
+  "`,
+    },
+    {
+      type: 'string',
+      content: 'kimaeyong',
+    },
+    {
+      type: 'rest',
+      content: '" -> $',
+    },
+    {
+      type: 'identifier',
+      content: 'pet_name',
+    },
+    {
+      type: 'rest',
+      content: '\n}\n',
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: '//',
+    },
+    {
+      type: 'comment',
+      content: ' "hi"\n',
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: '"',
+    },
+    {
+      type: 'string',
+      content: '// hi',
+    },
+    {
+      type: 'rest',
+      content: '";\n',
+    },
+  ],
+];
