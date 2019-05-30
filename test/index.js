bc0637b8b78024f226ebf522435b34e2ae47b723
Sejin Jeon <jinaidy93@gmail.com>
2019-05-30 22:54:54 +0900

    Add a test for transform

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 import chai from 'chai';
 import testParse from './parse';
+import testTransform from './transform';
 
 before(() => {
   chai.should();
 });
 
 testParse();
+testTransform();
---
+++ test/transform/testList/testDataList.js
@@ -0,0 +1,90 @@
+export default [
+  [
+    {
+      type: 'rest',
+      content: ':checkered_flag: :grapes:\n  :thought_balloon:',
+    },
+    {
+      type: 'comment',
+      content: ' Get things up and running here...',
+    },
+    {
+      type: 'rest',
+      content: '\n  :grinning: :abc:',
+    },
+    {
+      type: 'string',
+      content: 'Hello World!',
+    },
+    {
+      type: 'rest',
+      content: ':abc: ;\n:watermelon:\n',
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: 'main {\n  var ',
+    },
+    {
+      type: 'identifier',
+      content: 'pet_name',
+    },
+    {
+      type: 'rest',
+      content: ' : string  //',
+    },
+    {
+      type: 'comment',
+      content: ' I love cats.',
+    },
+    {
+      type: 'rest',
+      content: '\n  "',
+    },
+    {
+      type: 'string',
+      content: 'kimaeyong',
+    },
+    {
+      type: 'rest',
+      content: '" -> $ ',
+    },
+    {
+      type: 'identifier',
+      content: 'pet_name',
+    },
+    {
+      type: 'rest',
+      content: '\n}\n',
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: '//',
+    },
+    {
+      type: 'comment',
+      content: ' "hi"',
+    },
+    {
+      type: 'rest',
+      content: '\n',
+    },
+  ],
+  [
+    {
+      type: 'rest',
+      content: '"',
+    },
+    {
+      type: 'string',
+      content: '// hi',
+    },
+    {
+      type: 'rest',
+      content: '" ;\n',
+    },
+  ],
+];
